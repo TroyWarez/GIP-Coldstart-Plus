8ec36119d3e88420a030f608c0e0eb6e81c1aaf0
Troy <TroyWarez@troysdomain.com>
2025-08-19 16:29:07 -0700

    serial and mon mode

--- GIP_Coldstart+/main.cpp
+++ GIP_Coldstart+/main.cpp
@@ -39,46 +39,44 @@ void pcapCallback(u_char* arg_array, const struct pcap_pkthdr* h, const u_char* 
 
 }
 int main() {
-// 	const unsigned char beaconPacketData[80] = { 0x0, 0x0, 0x18, 0x0, 0x2b, 0x0, 0x0, 0x0, 0x7b, 0x84, 0xb5, 0x18, 0x0, 0x0, 0x0, 0x0, 0x10, 0x0, 0x99, 0x9, 0x0, 0x0, 0xb1, 0x00, 0x80, 0x0, 0x0, 0x0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x62, 0x45,
-// 
-// 		//Change these to your own values from "airodump-ng -c 1" every dongle mac address starts with 62:45
-// 		0xff, 0xff, 0xff, 0xff,
-// 
-// 		0x62, 0x45,
-// 
-// 		//Change these to your own values from "airodump-ng -c 1" every dongle mac address starts with 62:45
-// 		0xff, 0xff, 0xff, 0xff,
-// 
-// 		0xc0, 0xe9, 0x2f, 0x9f, 0xc2, 0x16, 0x0, 0x0, 0x0, 0x0, 0x64, 0x0, 0x31, 0xc6, 0x0, 0x0, 0xdd, 0xc, 0x0, 0x50, 0xf2, 0x11, 0x1, 0x10, 0x0, 0xa1, 0x28, 0x9d, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
-// 	size_t packet_size = sizeof(beaconPacketData);
-// 	pcap_if_t* dev; /* name of the device to use */
-// 	char errbuf[PCAP_ERRBUF_SIZE];
-// 	struct bpf_program fp_prog;
-// 	u_char* userHandle = NULL;
-// 	pcap_t* handle = NULL;
-// 	/* ask pcap to find a valid device for use to sniff on */
-// 	int ret = pcap_init(0, errbuf);
-// 	ret = pcap_findalldevs(&dev, errbuf);
-// 	handle = pcap_open_live(
-// 		"wlan0mon",
-// 		BUFSIZ,
-// 		1,
-// 		1,
-// 		errbuf
-// 	);
-// 	if (handle == NULL) {
-// 		fprintf(stderr, "pcap_open_live() failed: %s\n", errbuf);
-// 		return 1;
-// 	}
-// 	wiringPiSetupSys();
-// 
-// 	pinMode(LED, OUTPUT);
-// 
-// 	ret = pcap_compile(handle, &fp_prog, "wlan subtype assoc-req", 1, PCAP_NETMASK_UNKNOWN);
-// 	if (ret == -1) {
-// 		fprintf(stderr, "pcap_compile() failed: %s\n", pcap_geterr(handle));
-// 		return 1;
-// 	}
+	system("airmon-ng start wlan0");
+	system("airodump-ng -c 1");
+
+	const unsigned char beaconPacketData[80] = { 0x0, 0x0, 0x18, 0x0, 0x2b, 0x0, 0x0, 0x0, 0x7b, 0x84, 0xb5, 0x18, 0x0, 0x0, 0x0, 0x0, 0x10, 0x0, 0x99, 0x9, 0x0, 0x0, 0xb1, 0x00, 0x80, 0x0, 0x0, 0x0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x62, 0x45,
+
+		//Change these to your own values from "airodump-ng -c 1" every dongle mac address starts with 62:45
+		0xff, 0xff, 0xff, 0xff,
+
+		0x62, 0x45,
+
+		//Change these to your own values from "airodump-ng -c 1" every dongle mac address starts with 62:45
+		0xff, 0xff, 0xff, 0xff,
+
+		0xc0, 0xe9, 0x2f, 0x9f, 0xc2, 0x16, 0x0, 0x0, 0x0, 0x0, 0x64, 0x0, 0x31, 0xc6, 0x0, 0x0, 0xdd, 0xc, 0x0, 0x50, 0xf2, 0x11, 0x1, 0x10, 0x0, 0xa1, 0x28, 0x9d, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
+	size_t packet_size = sizeof(beaconPacketData);
+	pcap_if_t* dev; /* name of the device to use */
+	char errbuf[PCAP_ERRBUF_SIZE];
+	struct bpf_program fp_prog;
+	u_char* userHandle = NULL;
+	pcap_t* handle = NULL;
+	/* ask pcap to find a valid device for use to sniff on */
+	int ret = pcap_init(0, errbuf);
+	ret = pcap_findalldevs(&dev, errbuf);
+	handle = pcap_open_live(
+		"wlan0mon",
+		BUFSIZ,
+		1,
+		1,
+		errbuf
+	);
+	if (handle == NULL) {
+		fprintf(stderr, "pcap_open_live() failed: %s\n", errbuf);
+		return 1;
+	}
+	wiringPiSetupSys();
+
+	pinMode(LED, OUTPUT);
+
 
 
 	// Open the serial port. Change device path as needed (currently set to an standard FTDI USB-UART cable type device)
