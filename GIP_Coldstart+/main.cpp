65249ab8fad53dd8c188e5e778a535e338d36e5a
Troy <TroyWarez@troysdomain.com>
2025-09-24 20:19:07 -0700

    debug flag removed

--- GIP_Coldstart+/main.cpp
+++ GIP_Coldstart+/main.cpp
@@ -135,7 +135,6 @@ void pcapCallback(u_char* arg_array, const struct pcap_pkthdr* h, const u_char* 
 	if ( h->caplen >= 40) {
 		if (packet[34] == 0x7e && packet[35] == 0xed)
 		{
-			syncMode = true; // Debug, remove me
 			if (syncMode && !clearMode) {
 				syncMode = false;
 				if (!IsControllerAllowed(&packet[34]))
